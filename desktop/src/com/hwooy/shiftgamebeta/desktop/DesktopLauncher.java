f75a7d1704c89acbc58e15668fe616ce1ea366a5
Jason <jason.j.xiong@gmail.com>
2014-11-16 16:27:01 -0600

    Initial commit

---
+++ desktop/src/com/hwooy/shiftgamebeta/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.hwooy.shiftgamebeta.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.hwooy.shiftgamebeta.ShiftGameBeta;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ShiftGameBeta(), config);
+	}
+}
---
+++ html/src/com/hwooy/shiftgamebeta/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.hwooy.shiftgamebeta.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.hwooy.shiftgamebeta.ShiftGameBeta;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new ShiftGameBeta();
+        }
+}
