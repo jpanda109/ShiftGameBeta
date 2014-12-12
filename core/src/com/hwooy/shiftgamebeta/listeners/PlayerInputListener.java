02ac7a94657bba788da76395725ce6de9ec0df28
Jason <jason.j.xiong@gmail.com>
2014-12-11 23:05:23 -0600

    implemtnging keys for input on desktop

--- core/src/com/hwooy/shiftgamebeta/listeners/PlayerInputListener.java
+++ core/src/com/hwooy/shiftgamebeta/listeners/PlayerInputListener.java
@@ -1,7 +1,9 @@
 package com.hwooy.shiftgamebeta.listeners;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.math.Vector2;
 import com.hwooy.shiftgamebeta.screens.GameScreen;
 import com.hwooy.shiftgamebeta.utils.God;
 
@@ -21,6 +23,17 @@ public class PlayerInputListener extends InputAdapter {
         if (keycode == Input.Keys.SPACE) {
             gameScreen.shiftPlayer();
         }
+        if (keycode == Input.Keys.D) {
+            gameScreen.flingPlayer(300f, 300f);
+        }
+        if (keycode == Input.Keys.A) {
+            gameScreen.flingPlayer(-300f, 300f);
+        }
+        if (keycode == Input.Keys.W) {
+            Vector2 pos = gameScreen.player.body.getPosition();
+            gameScreen.flingPlayer((Gdx.input.getX() - 8 * pos.x) * 30, (320 - (Gdx.input.getY() - 8 * pos.y)) * 8);
+            System.out.println(320 - (Gdx.input.getY() - pos.y));
+        }
 
         // dev cheats
         if (God.DEBUG) {
@@ -30,4 +43,5 @@ public class PlayerInputListener extends InputAdapter {
         }
         return false;
     }
+
 }
